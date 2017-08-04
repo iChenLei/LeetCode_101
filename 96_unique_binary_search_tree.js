703ce698c95e8f9ca687570527805a5091dff25d
ichenlei <2470828450@qq.com>
2017-08-04 23:19:03 +0800

    *leetcode No.96 | whhat time I delete the file?

---
+++ 96_unique_binary_search_tree.js
@@ -0,0 +1,26 @@
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+/** Catalan number 
+ *  卡塔兰数没有斐波契那知名 
+ *  感觉这个 不像是靠算法 更像是数学
+ *  规律就是 Cn+1 = 求和Ci*Cn-i
+ */
+var numTrees = function(n) {
+    var array = new Array(n).fill(0);
+    array[0] = 1;
+    array[1] = 1;
+    
+    for(var i = 2;i<=n;++i){
+        //这里需要置零  比较重要的操作
+        array[i] = 0;
+        for(var j = 0;j<i;++j){
+            array[i] += array[j]*array[i-j-1];
+        }
+    }
+    
+    return array[n];
+};
